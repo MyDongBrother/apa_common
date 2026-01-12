aad7822999afee4cda8bc23a94870419536ca05f
jiandongliu <jiandong_liu@outlook.com>
2026-01-12 13:59:58 +0800

    修复车位类型的bug

--- MathLib.c
+++ MathLib.c
@@ -49,6 +49,32 @@ void Convert(const Pos2d_T *pO, const Pos2d_T *pin, Pos2d_T *pout)
 }
 
 /**
+ * @brief 将全局坐标系下的点转换到局部坐标系
+ *
+ * @param pO    局部坐标系原点在全局坐标系的位置及朝向 (x, y, phi)
+ * @param pin   全局坐标系下的点坐标  (x, y, phi)
+ * @param pout  输出的局部坐标系下的点坐标  (x, y, phi)
+ *
+ * @note 使用二维刚体逆变换：先平移，再旋转
+ */
+void RevConvert(const Pos2d_T *pO, const Pos2d_T *pin, Pos2d_T *pout)
+{
+    // 平移到局部原点
+    float dx = pin->x - pO->x;
+    float dy = pin->y - pO->y;
+
+    // 旋转到局部坐标系
+    float cos_phi = cosf(pO->phi);
+    float sin_phi = sinf(pO->phi);
+
+    pout->x = dx * cos_phi + dy * sin_phi;
+    pout->y = -dx * sin_phi + dy * cos_phi;
+
+    // phi
+    pout->phi = Round_PI(pin->phi - pO->phi);
+}
+
+/**
  * @brief 将局部坐标系下的点转换到全局坐标系
  *
  * @param pO    局部坐标系原点在全局坐标系的位置及朝向 (x, y, phi)
@@ -76,4 +102,16 @@ void ConvertArray(const float *pO, const float *pin, float *pout)
 {
     pout[0] = pin[0] * cosf(pO[2]) - pin[1] * sinf(pO[2]) + pO[0];
     pout[1] = pin[0] * sinf(pO[2]) + pin[1] * cosf(pO[2]) + pO[1];
+}
+
+void RevConvertArray(const float *pO, const float *pin, float *pout)
+{
+    float dx = pin[0] - pO[0];
+    float dy = pin[1] - pO[1];
+
+    float cos_phi = cosf(pO[2]);
+    float sin_phi = sinf(pO[2]);
+
+    pout[0] = dx * cos_phi + dy * sin_phi;
+    pout[1] = -dx * sin_phi + dy * cos_phi;
 }
